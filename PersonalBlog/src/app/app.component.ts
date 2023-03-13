ee1cf05288fa692338f8f6cbd8d705e338d84c9d
donghg201 <donghg201@gmail.com>
2023-03-13 22:53:17 +0700

    Push 12/02/2023

--- PersonalBlog/src/app/app.component.ts
+++ PersonalBlog/src/app/app.component.ts
@@ -7,5 +7,17 @@ import {Component, OnInit, ViewChild} from '@angular/core';
 })
 export class AppComponent {
   title = 'PersonalBlog';
+  private _event: any;
 
+  onActivate(event: any) {
+    this._event = event;
+    let scrollToTop = window.setInterval(() => {
+      let pos = window.pageYOffset;
+      if (pos > 0) {
+        window.scrollTo(0, pos - 20); // how far to scroll on each step
+      } else {
+        window.clearInterval(scrollToTop);
+      }
+    }, 16);
+  }
 }
